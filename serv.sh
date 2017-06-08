f87c48bcdc93bd6b206bbc4d3290e7e637f31db7
Dinor Geler <digeler@users.noreply.github.com>
2017-06-08 11:42:01 +0300

    Update serv.sh

--- serv.sh
+++ serv.sh
@@ -15,21 +15,4 @@ else
 fi
 sh -c 'until ping -c1 leader.mesos;do echo waiting for leader.mesos;sleep 15;done;echo leader.mesos up'
 
-echo ***************working on hv clock now 
-
-systemctl disable systemd-timesyncd
-
-sudo apt-get install ntp ntpdate -y
-
-sudo echo 2dd1ce17-079e-403c-b352-a1921ee207ee > /sys/bus/vmbus/drivers/hv_util/unbind
-
-sudo systemctl stop systemd-timesyncd
-
-sudo hwclock --systohc --utc
-sudo ntpdate -s ntp.ubuntu.com
-sudo service ntp stop
-sudo ntpd -gq
-sudo service ntp start
-sudo systemctl enable ntp
-echo am done
 
